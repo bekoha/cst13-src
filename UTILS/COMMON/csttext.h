d63c79bac202eba23424d1adbef11bfad8fba991
Tim <koire17@gmail.com>
2023-05-21 10:20:09 +0300

    import of cst13-src

---
+++ UTILS/COMMON/csttext.h
@@ -0,0 +1,18 @@
+//========= Copyright © 2005, amckern, All rights reserved. ====================//
+//
+// Purpose: All CST Message Code (that is used more then once)
+//
+// $NoKeywords: $
+//
+//=============================================================================//
+
+// Banner - The text that is displayed at the top of each tools screen out put
+
+// 32 Bit
+#define CST_BANNER "Custom Source Tools 1.3\nBuilt by Adam 'amckern' McKern on (" __DATE__ ")\nPlease submit Bugs or Ideas to amckern@yahoo.com\nCST Based on code by, Valve Software LLC\n\n"
+
+// Compilier Error site, useful when there is a common error, it can point the user to the website
+#define CST_ERROR_SITE "Common Complie errors can be found at http://www.interlopers.net/errors/ \n"
+
+// Advert - useful for sponsers, or promoting a new tool / website
+#define CST_ADVERT "The best stuff is always the worst to clean up!\n\n"
